6152c3e46590c6cd85a4a93b90a5d0ce6e6f3f18
Sabid Farhan <sfttanim@gmail.com>
2024-12-20 11:05:27 +0600

    blog creation page design complete

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -7,7 +7,7 @@ const Root = () => {
   return (
     <div className="font-mont" data-theme={darkTheme ? "dark" : "light"}>
       <Navber></Navber>
-      <div className="container mx-auto min-h-screen px-2 lg:px-0 mt-8">
+      <div className="container mx-auto min-h-screen px-2 lg:px-0 mt-8 lg:mt-14">
         <Outlet></Outlet>
       </div>
     </div>
--- src/pages/CreateBlog.jsx
+++ src/pages/CreateBlog.jsx
@@ -1,20 +1,64 @@
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import PageTitle from "../components/PageTitle";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const CreateBlog = () => {
   return (
     <div className="flex flex-col-reverse lg:flex-row">
+      {/* Animation */}
       <DotLottieReact
         src="https://lottie.host/7ee26a40-3808-4826-a1a5-de8af7083974/73w9BXYyAY.lottie"
         loop
         autoplay
         className="w-full lg:w-1/3 flex-1 mt-10 lg:mt-0"
-      />{" "}
+      />
+
+      {/* Form Section */}
       <div className="flex-1">
         <PageTitle
           heading={"Blog Creations Hub"}
           subHeading={"Create your blog effortlessly with the power of AI."}
         ></PageTitle>
+        <div className="mt-2">
+          <Formik
+            initialValues={{ message: "", password: "" }}
+            validate={(values) => {
+              const errors = {};
+              if (!values.message) {
+                errors.message = "*Required";
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                console.log(values.message);
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <Field
+                  className="input input-bordered w-full min-h-32"
+                  as="textarea"
+                  name="message"
+                />
+                <div className="text-red-700 text-sm w-full flex justify-center lg:justify-end mb-2">
+                  <ErrorMessage name="message" component="div" className="" />
+                </div>
+                <div className="flex justify-center lg:justify-end ">
+                  <button
+                    className="button-style "
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </div>
     </div>
   );
