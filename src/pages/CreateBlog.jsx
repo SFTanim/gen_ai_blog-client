cc363893e31d1c623c219e9b606319c3302d5f40
Sabid Farhan <sfttanim@gmail.com>
2024-12-21 07:32:14 +0600

    creating a blog with the help of google gemini ai has been successfully implemented

--- src/pages/CreateBlog.jsx
+++ src/pages/CreateBlog.jsx
@@ -1,8 +1,33 @@
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import PageTitle from "../components/PageTitle";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import useAxiosPublic from "./../hooks/useAxiosPublic";
+import { useEffect, useState } from "react";
 
 const CreateBlog = () => {
+  const axiosPublic = useAxiosPublic();
+  const [input, setInput] = useState("");
+  const [aiData, setAiData] = useState([]);
+
+  useEffect(() => {
+    if (input) {
+      const fetchData = async () => {
+        const res = await axiosPublic.post("/api/suggest", {
+          input,
+        });
+        console.log("response from fetching: ", res);
+        if (res?.data) {
+          console.log(res?.data?.title);
+          console.log(res?.data?.description);
+          setAiData(aiData);
+        }
+      };
+      fetchData();
+    }
+  }, [input, axiosPublic, aiData]);
+
+  console.log(aiData);
+
   return (
     <div className="flex flex-col-reverse lg:flex-row">
       {/* Animation */}
@@ -10,18 +35,20 @@ const CreateBlog = () => {
         src="https://lottie.host/7ee26a40-3808-4826-a1a5-de8af7083974/73w9BXYyAY.lottie"
         loop
         autoplay
-        className="w-full lg:w-1/3 flex-1 mt-10 lg:mt-0"
+        className="w-full lg:w-1/3 flex-1 mt-16 lg:mt-0"
       />
 
       {/* Form Section */}
       <div className="flex-1">
         <PageTitle
           heading={"Blog Creations Hub"}
-          subHeading={"Create your blog effortlessly with the power of AI."}
+          subHeading={
+            "Type a paragraph about your blog topic or idea, and we'll generate a catchy title and description for you!"
+          }
         ></PageTitle>
         <div className="mt-2">
           <Formik
-            initialValues={{ message: "", password: "" }}
+            initialValues={{ message: "" }}
             validate={(values) => {
               const errors = {};
               if (!values.message) {
@@ -29,30 +56,31 @@ const CreateBlog = () => {
               }
               return errors;
             }}
-            onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                console.log(values.message);
-                setSubmitting(false);
-              }, 400);
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              setInput(values?.message);
+              console.log(values?.message);
+              resetForm();
+              setSubmitting(false);
             }}
           >
             {({ isSubmitting }) => (
               <Form>
                 <Field
-                  className="input input-bordered w-full min-h-32"
+                  className="input input-bordered w-full min-h-32 pt-2"
                   as="textarea"
                   name="message"
+                  placeholder="Enter a paragraph about your blog topic..."
                 />
                 <div className="text-red-700 text-sm w-full flex justify-center lg:justify-end mb-2">
                   <ErrorMessage name="message" component="div" className="" />
                 </div>
-                <div className="flex justify-center lg:justify-end ">
+                <div className="flex justify-center lg:justify-end">
                   <button
-                    className="button-style "
+                    className="button-style w-full"
                     type="submit"
                     disabled={isSubmitting}
                   >
-                    Submit
+                    Generate with AI {/* Updated button text */}
                   </button>
                 </div>
               </Form>
